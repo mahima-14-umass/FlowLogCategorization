06f93db5eefa659d7f7752de3ce9e04b35cf41c9
mahima-umass <mahima.choudha@gmail.com>
2025-02-22 21:31:56 -0600

    Fixed NoSuchFile Exception

--- src/test/java/LookupTableLoaderTest.java
+++ src/test/java/LookupTableLoaderTest.java
@@ -1,12 +1,11 @@
 import org.flc.LookupTableLoader;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.flc.Constants.LOOKUP_FILE;
+import static org.flc.Constants.BASE_PATH_TEST;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.doReturn;
@@ -19,19 +18,19 @@ public class LookupTableLoaderTest {
         LookupTableLoader lookupTableLoader = spy(new LookupTableLoader());
         Map<String, String> mockLookup = new HashMap<>();
         mockLookup.put("443,tcp", "sv_P2");
-        doReturn(mockLookup).when(lookupTableLoader).loadLookupTable(LOOKUP_FILE);
-        assertEquals("sv_P2", lookupTableLoader.loadLookupTable(LOOKUP_FILE).get("443,tcp"));
+        doReturn(mockLookup).when(lookupTableLoader).loadLookupTable(BASE_PATH_TEST + "lookup.csv");
+        assertEquals("sv_P2", lookupTableLoader.loadLookupTable(BASE_PATH_TEST + "lookup.csv").get("443,tcp"));
     }
 
     @Test
     void testLoadLookupTable() throws IOException {
-        Map<String, String> lookupTable = LookupTableLoader.loadLookupTable("lookup.csv");
+        Map<String, String> lookupTable = LookupTableLoader.loadLookupTable(BASE_PATH_TEST + "lookup.csv");
         assertNotNull(lookupTable, "Lookup table should not be null");
     }
 
     @Test
     void testLoadProtocolTable() throws IOException {
-        Map<String, String> protocolTable = LookupTableLoader.loadProtocolTable("protocol-numbers-1.csv");
+        Map<String, String> protocolTable = LookupTableLoader.loadProtocolTable(BASE_PATH_TEST + "protocol-numbers-1.csv");
         assertNotNull(protocolTable, "Protocol table should not be null");
     }
 }
