388594e9244eb8371ff0e9a2b90a353ae30ff941
mahima-umass <mahima.choudha@gmail.com>
2025-02-23 14:39:16 -0600

    Added test case for ReadFile method

--- src/test/java/FileUtilTest.java
+++ src/test/java/FileUtilTest.java
@@ -13,12 +13,12 @@ import static org.flc.Constants.BASE_PATH_TEST;
 public class FileUtilTest {
 
     @Test
-    void testFileUtil() throws IOException {
+    void testWriteOutputAndReadFile() throws IOException {
         String testFile = BASE_PATH_TEST + "test_output.csv";
         List<String> testData = Arrays.asList("Tag,Count", "sv_P1,2", "email,1");
         FileUtil.writeOutput(testFile, testData);
 
-        List<String> readData = Files.readAllLines(Paths.get(testFile));
+        List<String> readData = FileUtil.readFile(testFile);
         assertEquals(testData, readData);
     }
 }
